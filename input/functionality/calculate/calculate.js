b94e745d8062d37371ae99e68211877749e2a92f
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-09-28 18:06:53 -0300

    make the calculate button work

--- input/functionality/calculate/calculate.js
+++ input/functionality/calculate/calculate.js
@@ -1,2 +1,37 @@
 const calculateButton = document.querySelector(`#resultContent button`);
 
+calculateButton.addEventListener('click', calculate)
+
+
+
+function calculate() {
+
+let result =  currentList.reduce((accumulator, currentItem) => {
+    switch (currentItem.type) {
+      case 'food':
+        return accumulator + calculatedFood(currentItem);
+      case 'product':
+      case 'money':
+        return accumulator + currentItem.price(currentItem);
+    }
+  }, 0);
+  console.log(result)
+}
+
+
+
+function calculatedFood(currentFood) {
+  let priceForUnit = currentFood.price / currentFood.amountPerPrice;
+  let amountOfFoodEatenPerMonthOrWeek = currentFood.amountPerDay * getProcesure(currentFood);
+  let averageFood = priceForUnit * amountOfFoodEatenPerMonthOrWeek;
+  return averageFood;
+}
+
+function getProcesure(currentFood){
+if (typeOfCalculation == 'monthly') {
+return currentFood.monthAmount
+}
+else {
+return currentFood.weekAmount
+}
+}
