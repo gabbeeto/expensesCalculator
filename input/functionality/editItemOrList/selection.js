e0d890d0297e5ecd871f3f0c52cc4b1f32fbb5f2
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-09-29 17:12:21 -0300

    convert the buttons into a select

--- input/functionality/editItemOrList/selection.js
+++ input/functionality/editItemOrList/selection.js
@@ -1,12 +1,12 @@
 export function selectDiv(event) {
   if (event.target.dataset.index) {
     window.selectedIndex = event.target.dataset.index
-    selectParent()
+    selectParent(event)
   }
 }
 
 
-function selectParent(){
+function selectParent(event){
 let parent = event.target.parentNode;
 let selectedItems = document.querySelectorAll('.selectedItem');
 selectedItems.forEach( element => element.classList.remove('selectedItem'));
