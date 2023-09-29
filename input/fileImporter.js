651022cb3c3e7e652be3746a3904da3a51094f60
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-09-29 19:03:41 -0300

    add 2 dialogs

--- input/fileImporter.js
+++ input/fileImporter.js
@@ -6,11 +6,13 @@ import './styling/darkmode.js'
 import './styling/calculationType.css'
 import './styling/list.css'
 import './styling/desktopSupport.css'
+import './styling/popup.css'
 
 
 // import functionality
 import './functionality/list/listStructure.js';
 import './functionality/list/addItemsToList.js';
+import './functionality/window/createList.js';
 import './functionality/display/expenseType.js';
 import './functionality/typeCalculator.js';
 import './functionality/calculate/calculate.js';
