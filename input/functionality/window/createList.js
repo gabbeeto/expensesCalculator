8bf2a1a3d74345ff888b1941c06a06b47b0e39a9
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-09-30 15:55:21 -0300

    generate the content for the product and money on the edit dialog

--- input/functionality/window/createList.js
+++ input/functionality/window/createList.js
@@ -13,36 +13,52 @@ export function createList() {
 function generateContentForDialog() {
   let typeOfList = document.querySelector('#itemOrListContainer').value;
   let item = currentList.array[selectedIndex];
-
   if (typeOfList == 'item') {
-    dialog.innerHTML = `
-<button class='closeBtn'>close</button>
-<p>name:</p>
-<input value='${item.name}'type="text" id="name">
-<p>price:</p>
-<input value='${item.price}' type="text" inputmode='decimal' id="price">
-<p>amount of food per price:</p>
-<input  value='${item.amountPerPrice}' type="text" inputmode='numeric' id="amountPerPrice">
-<p>amount of food per day:</p>
-<input   value='${item.amountPerDay}' type="text" inputmode='numeric' id="amountPerDay">
-<div>
-  <section>
-    <p>amount of days per month: <br> <span>(you eat)</span></p>
-    <input value='${item.monthAmount}' type="text" inputmode='numeric' id="monthAmount">
-  </section>
-  <p>or</p>
-  <section>
-    <p>amount of days per week: <br> <span>(you eat)</span></p>
-    <input value='${item.weekAmount}' type="text" inputmode='numeric' id="weekAmount">
-  </section>
-</div>
-<button type='button' id='apply'>apply</button>
-`
-let applyButton = dialog.querySelector('#apply');
-applyButton.addEventListener('click', applyChanges);
+    switch (item.type) {
+      case 'food':
+        dialog.innerHTML = `
+        <button class='closeBtn'>close</button>
+        <p>name:</p>
+        <input value='${item.name}'type="text" id="name">
+        <p>price:</p>
+        <input value='${item.price}' type="text" inputmode='decimal' id="price">
+        <p>amount of food per price:</p>
+        <input  value='${item.amountPerPrice}' type="text" inputmode='numeric' id="amountPerPrice">
+        <p>amount of food per day:</p>
+        <input   value='${item.amountPerDay}' type="text" inputmode='numeric' id="amountPerDay">
+        <div>
+          <section>
+            <p>amount of days per month: <br> <span>(you eat)</span></p>
+            <input value='${item.monthAmount}' type="text" inputmode='numeric' id="monthAmount">
+          </section>
+          <p>or</p>
+          <section>
+            <p>amount of days per week: <br> <span>(you eat)</span></p>
+            <input value='${item.weekAmount}' type="text" inputmode='numeric' id="weekAmount">
+          </section>
+        </div>
+        <button type='button' id='apply'>apply</button>`;
+        break;
+      case 'product':
+        dialog.innerHTML = `<button class='closeBtn'>close</button>
+        <p>name:</p>
+        <input value='${item.name}'type="text" id="name">
+        <p>price:</p>
+        <input value='${item.price}' type="text" inputmode='decimal' id="price">
+        <button type='button' id='apply'>apply</button>`;
+        break;
+      case 'money':
+        dialog.innerHTML = `<button class='closeBtn'>close</button>
+        <p>price:</p>
+        <input value='${item.price}' type="text" inputmode='decimal' id="price">
+        <button type='button' id='apply'>apply</button>`;
+        break;
+    }
+    let applyButton = dialog.querySelector('#apply');
+    applyButton.addEventListener('click', applyChanges);
   }
 }
 
-function applyChanges(){
-alert('changes are being apppliieid');
+function applyChanges() {
+  alert('changes are being apppliieid');
 }
