1e7f4e82e6dcc61a63d20655e8492afdb74c3767
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-09-30 15:05:24 -0300

    add the ability to open and close the dialogs

--- input/functionality/window/createList.js
+++ input/functionality/window/createList.js
@@ -1,1 +1,31 @@
-alert('file is working');
+const editButton = document.querySelector('#listContainer button');
+const addNewListButton = document.querySelector('#listContainer button:nth-of-type(2)');
+
+const addNewListDialog = document.getElementById('addNewListPopUp');
+const editDialog = document.getElementById('editPopUp');
+
+
+for (let closeBtn of document.querySelectorAll('.closeBtn')) {
+  closeBtn.addEventListener('click', closePopUp)
+}
+
+editButton.addEventListener('click', () => {
+  openWindow(editDialog)
+}
+);
+addNewListButton.addEventListener('click', () => {
+  openWindow(addNewListDialog)
+}
+);
+
+function openWindow(dialog) {
+  dialog.showModal()
+}
+
+function closePopUp() {
+  for (let dialog of document.querySelectorAll('dialog')) {
+    if (dialog.open) {
+      dialog.close()
+    }
+  }
+}
